f6faf67a6e0ae19c672230c66b94900f5885e890
TOUNGA Franck <ntounga@gmail.com>
2015-10-16 08:13:39 +0200

    rajout de toutes les proprietes dans le fichier persistence xml

--- src/main/java/com/masterheboss/repository/GenericRepository.java
+++ src/main/java/com/masterheboss/repository/GenericRepository.java
@@ -36,11 +36,14 @@ public abstract class GenericRepository<T extends Identifiable<PK>, PK> {
   }
 
   public List<T> findAll() {
-    EntityManagerFactory Factory = Persistence.createEntityManagerFactory("HibernateProject");;
-    entityManagerFactory.createEntityManager();
-    CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+    EntityManagerFactory Factory = Persistence.createEntityManagerFactory("HibernateProject");
+    EntityManager e=  Factory.createEntityManager();
+    //entityManagerFactory.createEntityManager();
+    //CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+    CriteriaQuery cq = e.getCriteriaBuilder().createQuery();
     cq.select(cq.from(entityClass));
-    return em.createQuery(cq).getResultList();
+    //return em.createQuery(cq).getResultList();
+    return e.createQuery(cq).getResultList();
   }
 
 
